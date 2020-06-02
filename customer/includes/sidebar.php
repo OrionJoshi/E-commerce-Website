05c2812268688844c0f38f7de68be2765f4314e3
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-02 18:30:53 +0545

    Customer sidebar add

--- customer/includes/sidebar.php
+++ customer/includes/sidebar.php
@@ -15,6 +15,31 @@
                     <i class="fa fa-list"></i> My Orders
                 </a>
             </li>
+            <li class="<?php if(isset($_GET['pay_offline'])){ echo "active";} ?>">
+                <a href="my_account.php?pay_offline">
+                    <i class="fa fa-bolt"></i> Pay Offline
+                </a>
+            </li>
+            <li class="<?php if(isset($_GET['edit_account'])){ echo "active";} ?>">
+                <a href="my_account.php?edit_account">
+                    <i class="fa fa-pencil"></i> Edit Account
+                </a>
+            </li>
+            <li class="<?php if(isset($_GET['change_pass'])){ echo "active";} ?>">
+                <a href="my_account.php?change_pass">
+                    <i class="fa fa-user"></i> Change Password
+                </a>
+            </li>
+            <li class="<?php if(isset($_GET['delete_account'])){ echo "active";} ?>">
+                <a href="my_account.php?delete_account">
+                    <i class="fa fa-trash-o"></i> Delete Account
+                </a>
+            </li>
+            <li>
+                <a href="logout.php">
+                    <i class="fa fa-sign-out"></i> Log Out
+                </a>
+            </li>
         </ul>
     </div>
 </div>
