3cd34452ee5dea35b2d0b94cf598b922c9480816
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-02 18:21:39 +0545

    Customer profile add

--- customer/includes/sidebar.php
+++ customer/includes/sidebar.php
@@ -1,30 +1,20 @@
 <div class="panel panel-default sidebar-menu">
     <div class="panel-heading">
-        <h3 class="panel-title">Products Categories</h3>
+        <center>
+            <img src="customer_images/MrJerk.jpg" alt="M-Dev Profile">
+        </center>
+        <br>
+        <h3 align="center" class="panel-title">
+            Name: MrJerk
+        </h3>
     </div>
     <div class="panel-body">
-        <ul class="nav nav-pills nav-stacked category-menu">
-            <li><a href="#">Jackets</a></li>
-            <li><a href="#">Accessories</a></li>
-            <li><a href="#">Shoes</a></li>
-            <li><a href="#">Coats</a></li>
-            <li><a href="#">T-Shirts</a></li>
+        <ul class="nav-pills nav-stacked nav">
+            <li class="<?php if(isset($_GET['my_orders'])){ echo "active";} ?>">
+                <a href="my_account.php?my_orders">
+                    <i class="fa fa-list"></i> My Orders
+                </a>
+            </li>
         </ul>
     </div>
-
-</div>
-
-<div class="panel panel-default sidebar-menu">
-    <div class="panel-heading">
-        <h3 class="panel-title">Categories</h3>
-    </div>
-    <div class="panel-body">
-        <ul class="nav nav-pills nav-stacked category-menu">
-            <li><a href="#">Men</a></li>
-            <li><a href="#">Women</a></li>
-            <li><a href="#">Kids</a></li>
-            <li><a href="#">Others</a></li>
-        </ul>
-    </div>
-
 </div>
