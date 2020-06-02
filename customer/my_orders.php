cab259b5db5379e3d94c81243e0a46fa2df2184b
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-02 19:21:01 +0545

    Customer orders add

---
+++ customer/my_orders.php
@@ -0,0 +1,76 @@
+<center>
+
+    <h1> My Orders </h1>
+
+    <p class="lead"> Your orders on one place</p>
+
+    <p class="text-muted">
+        If You have any questions, feel free to <a href="../contact.php">contact us</a>. Our Customer service work <strong>24/7</strong>
+    </p>
+
+</center>
+
+<hr>
+
+<div class="table-responsive">
+
+    <table class="table table-bordered table-hover">
+        <thead>
+            <tr>
+                <th> ON: </th>
+                <th> Due Amount: </th>
+                <th> Invoice No: </th>
+                <th> Qty: </th>
+                <th> Size: </th>
+                <th> Order Date: </th>
+                <th> Paid / Unpaid: </th>
+                <th> Status: </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th> #1 </th>
+
+                <td> $80 </td>
+                <td> 3639862 </td>
+                <td> 2 </td>
+                <td> Small </td>
+                <td> 10-01-2018 </td>
+                <td> Unpaid </td>
+
+                <td>
+                    <a href="confirm.php" target="_blank" class="btn btn-primary btn-sm"> Confirm Paid </a>
+                </td>
+            </tr>
+            <tr>
+                <th> #1 </th>
+
+                <td> $80 </td>
+                <td> 3639862 </td>
+                <td> 2 </td>
+                <td> Small </td>
+                <td> 10-01-2018 </td>
+                <td> Unpaid </td>
+
+                <td>
+                    <a href="confirm.php" target="_blank" class="btn btn-primary btn-sm"> Confirm Paid </a>
+                </td>
+            </tr>
+            <tr>
+                <th> #1 </th>
+
+                <td> $80 </td>
+                <td> 3639862 </td>
+                <td> 2 </td>
+                <td> Small </td>
+                <td> 10-01-2018 </td>
+                <td> Unpaid </td>
+
+                <td>
+                    <a href="confirm.php" target="_blank" class="btn btn-primary btn-sm"> Confirm Paid </a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+</div>
